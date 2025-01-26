58bbf3dad1ffae99bf0075e35fc04e2e2469bd8e
Cyletix <1016120209@qq.com>
2025-01-26 23:20:50 +0900

    vault backup: 2025-01-26 23:20:50

--- _assert_/scripts/add_displaystyle.py
+++ _assert_/scripts/add_displaystyle.py
@@ -1,21 +1,22 @@
 import re
+from pathlib import Path
 
 def add_displaystyle(file_path):
-    # 正则表达式模式
-    pattern = r'\$(?!\\displaystyle)(?=[^$]*?(\\lim|\\sum|\\int|\\Sigma)[^$]*?\$)'
-    
-    # 读取文件内容
     with open(file_path, 'r', encoding='utf-8') as f:
         content = f.read()
     
-    # 进行替换
-    new_content = re.sub(pattern, r'$\\displaystyle', content)
+    # 正则匹配单行公式，添加displaystyle
+    pattern = r'\$(?!\\displaystyle)(?=[^$]*?(\\lim|\\sum|\\int|\\Sigma)[^$]*?\$)'
+    content = re.sub(pattern, '$\\displaystyle', content)
     
-    # 写回文件
     with open(file_path, 'w', encoding='utf-8') as f:
-        f.write(new_content)
+        f.write(content)
 
-if __name__ == "__main__":
-    # 要处理的文件路径
-    file_path = "Other/例题/高数1真题/数一2010.md"
-    add_displaystyle(file_path)
+def process_files(base_dir):
+    for file_path in base_dir.rglob('**/数一20*.md'):
+        add_displaystyle(file_path)
+        print(f'Processed: {file_path}')
+
+if __name__ == '__main__':
+    base_dir = Path('d:/OneDrive/Notebook/Library/Science/Math')
+    process_files(base_dir)
